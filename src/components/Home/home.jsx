572e8a5d9d7e13845c9fda1937174fd16ed83ccc
Gustavo Loyola <guga.loyola17@gmail.com>
2024-08-20 17:52:35 -0300

    Cores de bg alteradas

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -3,11 +3,11 @@ import Img_Guh from '../../assets/img/Gustavo Loyola.png'
 
 const Home = () => {
     return (
-        <section className="bg-gradient-to-br from-dark-purple to-dark-gray h-screen">
+        <section className="bg-black-russian h-screen">
             <div className="flex justify-center flex-col items-center gap-10 h-full md:flex-row">
                 <img src={Img_Guh} alt="Imagem de Gustavo loyola" className="bg-blue-300 w-56 h-56 md:w-80 md:h-80 rounded-full" />
-                <h1 className="text-smoky-white text-xl md:text-3xl text-center">Olá, eu sou Gustavo Loyola <br/>
-                    <ReactTyped strings={['Desenvolvedor Front End']} typeSpeed={100} loop className="text-purple-500" />
+                <h1 className="text-smoky-white text-xl md:text-3xl text-center">Olá, eu sou Gustavo Loyola <br />
+                    <ReactTyped strings={['Desenvolvedor Front End']} typeSpeed={100} loop className="text-blue-violet" />
                 </h1>
             </div>
         </section>
