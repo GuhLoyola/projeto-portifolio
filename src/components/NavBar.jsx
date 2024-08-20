dccd42e1630b532a07a49bf176febd7cffa31c77
Gustavo Loyola <guga.loyola17@gmail.com>
2024-08-20 17:53:01 -0300

    Estilização alterada afim de otimizar o responsivo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,13 @@
 const NavBar = () => {
     return (
-        <nav className="bg-almost-black h-16 w-full text-smoky-white flex items-center">
+        <nav className="bg-dove-gray h-16 w-full text-smoky-white flex items-center  md:justify-between px-8">
             <div>
                 <a href="/" className="p-4">Gustavo Loyola</a>
             </div>
-            <ul className="hidden md:block">
-                <li>Sobre mim</li>
-                <li>Projetos</li>
-                <li>Contato</li>
+            <ul className="hidden md:flex md:gap-5">
+                <li><a href="/sobre-mim">Sobre mim</a></li>
+                <li><a href="/projetos">Projetos</a></li>
+                <li><a href="/contato">Contato</a></li>
             </ul>
         </nav>
     )
