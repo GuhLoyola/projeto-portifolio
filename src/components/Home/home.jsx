bb3626964aded4bf5890b3a878648e65aa726fc5
Gustavo Loyola <guga.loyola17@gmail.com>
2024-08-20 16:43:59 -0300

    Componente da página home criado

---
+++ src/components/Home/home.jsx
@@ -0,0 +1,17 @@
+import { ReactTyped } from "react-typed"
+import Img_Guh from '../../assets/img/Gustavo Loyola.png'
+
+const Home = () => {
+    return (
+        <section className="bg-gradient-to-br from-dark-purple to-dark-gray h-screen">
+            <div className="flex justify-center flex-col items-center gap-10 h-full md:flex-row">
+                <img src={Img_Guh} alt="Imagem de Gustavo loyola" className="bg-blue-300 w-56 h-56 md:w-80 md:h-80 rounded-full" />
+                <h1 className="text-smoky-white text-xl md:text-3xl text-center">Olá, eu sou Gustavo Loyola <br/>
+                    <ReactTyped strings={['Desenvolvedor Front End']} typeSpeed={100} loop className="text-purple-500" />
+                </h1>
+            </div>
+        </section>
+    )
+}
+
+export default Home
