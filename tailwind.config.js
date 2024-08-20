5d4f048d7cda35eca913f0b42300fc30c6fc45bf
Gustavo Loyola <guga.loyola17@gmail.com>
2024-08-20 17:52:00 -0300

    Paleta de cores adicionada

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        "dark-purple": "#1e1724",
-        "dark-gray": "#121212",
-        "almost-black": "#1C1C1C",
-        "smoky-white": "#F5F5F5"
+        "black-russian": "#121212",
+        "cod-gray": "#282828",
+        "mine-shaft": "#3f3f3f",
+        "dove-gray": "#575757",
+        "boulder": "#717171",
+        "gray-light": "#8b8b8b",
+        "blue-violet": "#8a2be2",
+        "medium-purple": "#9a48e6",
+        "amethyst": "#a960ea",
+        "lavender": "#b777ee",
+        "bright-lavender": "#c58ef1",
+        "alice-blue": "#d1e4f4",
+        'smoky-white': "#f5f5f5"
       }
     },
   },
