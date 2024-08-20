84444d7e61cd89977e02f0c1b53237375a03e99f
Gustavo Loyola <guga.loyola17@gmail.com>
2024-08-20 16:43:18 -0300

    Adicionado caminho padrão no router dom

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,11 +1,12 @@
-import { BrowserRouter, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./HomePage"
 
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-
+                <Route path="/" element={<HomePage />} />
             </Routes>
         </BrowserRouter>
     )
