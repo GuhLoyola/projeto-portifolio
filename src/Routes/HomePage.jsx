449040cb02fb93a0c4d8426c2ccc9c742b6987cd
Gustavo Loyola <guga.loyola17@gmail.com>
2024-08-20 16:44:24 -0300

    Rota de HomePage criada

---
+++ src/Routes/HomePage.jsx
@@ -0,0 +1,13 @@
+import Home from "../components/Home/home"
+import NavBar from "../components/NavBar"
+
+const HomePage = () => {
+    return (
+        <>
+            <NavBar />
+            <Home />
+        </>
+    )
+}
+
+export default HomePage
