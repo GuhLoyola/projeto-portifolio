d8c56edfb9a4d48b35596298b4f8f70f8f91a21f
Gustavo Loyola <guga.loyola17@gmail.com>
2024-08-20 16:42:07 -0300

    Novas cores adicionadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ export default {
     extend: {
       colors: {
         "dark-purple": "#1e1724",
-        "dark-gray": "#121212"
+        "dark-gray": "#121212",
+        "almost-black": "#1C1C1C",
+        "smoky-white": "#F5F5F5"
       }
     },
   },
