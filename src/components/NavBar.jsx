2f2709d9e1c58d0f9cf459c04d3fecd87ab64747
Gustavo Loyola <guga.loyola17@gmail.com>
2024-08-21 01:27:36 -0300

    Componente de NavBar aprimorado

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,66 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+
 const NavBar = () => {
+
+    const [navExpanded, setNavExpanded] = useState(false)
+    const [navColor, setNavColor] = useState(false)
+
+    const handleScroll = () => {
+        window.scrollY >= 20 ? setNavColor(true) : setNavColor(false)
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
-        <nav className="bg-dove-gray h-16 w-full text-smoky-white flex items-center  md:justify-between px-8">
-            <div>
-                <a href="/" className="p-4 font-bold">Gustavo Loyola</a>
+        <nav className={`fixed w-full z-50 top-0 transition-colors duration-300 
+        ${navColor ? 'bg-black-russian-66 backdrop-blur-md shadow-md' : 'bg-transparent backdrop-blur-md'}`}>
+            <div className="mx-auto p-4 flex justify-between items-center text-smoky-white">
+                <div className="flex items-center">
+                    <Link to='/'>
+                        <p className="uppercase font-bold">Gustavo Loyola</p>
+                    </Link>
+                </div>
+                <div className="md:hidden" onClick={() => setNavExpanded(!navExpanded)}>
+                    <div className="space-y-2">
+                        <span className={`block w-8 h-0.5 bg-smoky-white transition-transform duration-300 ease-in-out 
+                            ${navExpanded ? 'rotate-45 translate-y-2' : ''}`}></span>
+
+                        <span className={`block w-8 h-0.5 bg-smoky-white transition-opacity duration-300 ease-in-out
+                            ${navExpanded ? 'opacity-0' : ''}`}></span>
+
+                        <span className={`block w-8 h-0.5 bg-smoky-white transition-transform duration-300 ease-in-out 
+                            ${navExpanded ? '-rotate-45 -translate-y-3' : ''}`}></span>
+                    </div>
+                </div>
+                <div className={`${navExpanded ? "block" : "hidden"} md:flex md:items-center md:space-x-6`}>
+                    <ul className="flex flex-col md:flex-row md:items-center gap-4 p-4">
+                        <li>
+                            <Link to='/sobre-mim' className="text-smoky-white flex items-center gap-1"
+                                onClick={() => setNavExpanded(false)}>
+                                Sobre mim
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/projetos' className="text-white flex items-center gap-1"
+                                onClick={() => setNavExpanded(false)}>
+                                Projetos
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/contato' className="text-white flex items-center gap-1"
+                                onClick={() => setNavExpanded(false)}>
+                                Contato
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <ul className="hidden md:flex md:gap-5">
-                <li><a href="/sobre-mim">Sobre mim</a></li>
-                <li><a href="/projetos">Projetos</a></li>
-                <li><a href="/contato">Contato</a></li>
-            </ul>
         </nav>
     )
 }
