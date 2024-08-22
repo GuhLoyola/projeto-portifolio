c3d1e5f04e1318e80ee00e6efcf5a8cfa6f050c8
Gustavo Loyola <guga.loyola17@gmail.com>
2024-08-22 02:40:18 -0300

    Adicionado z-index de 30 para a imagem

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <section className="bg-black-russian h-screen">
             <div className="flex justify-center flex-col items-center gap-10 h-full md:flex-row">
-                <img src={Img_Guh} alt="Imagem de Gustavo loyola" className="bg-blue-300 w-56 h-56 md:w-80 md:h-80 rounded-full" />
+                <img src={Img_Guh} alt="Imagem de Gustavo loyola" className="bg-blue-300 w-56 h-56 md:w-80 md:h-80 rounded-full z-30" />
                 <h1 className="text-smoky-white text-xl md:text-3xl text-center">Olá, eu sou Gustavo Loyola <br />
                     <ReactTyped strings={['Desenvolvedor Front End']} typeSpeed={100} loop className="text-blue-violet" />
                 </h1>
