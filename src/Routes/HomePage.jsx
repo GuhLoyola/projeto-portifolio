50345b62d38fcaae2cc1a6e8e4de941e188e0312
Gustavo Loyola <guga.loyola17@gmail.com>
2024-08-21 22:47:11 -0300

    Componente de Starfield adicionado a homepage

--- src/Routes/HomePage.jsx
+++ src/Routes/HomePage.jsx
@@ -1,9 +1,11 @@
+import Starfield from "react-starfield"
 import Header from "../components/Header"
 import Home from "../components/Home/home"
 
 const HomePage = () => {
     return (
         <>
+            <Starfield starCount={1000} starColor={[255, 255, 255]} speedFactor={0.05} backgroundColor="transparent"/>
             <Header />
             <main>
                 <Home />
