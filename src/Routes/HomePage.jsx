f1a4cc52374b6cfb28148ebdea1d6c74f50829f0
Gustavo Loyola <guga.loyola17@gmail.com>
2024-08-22 02:39:56 -0300

    Prop 'backgroundColor' removida de starfield

--- src/Routes/HomePage.jsx
+++ src/Routes/HomePage.jsx
@@ -5,7 +5,7 @@ import Home from "../components/Home/home"
 const HomePage = () => {
     return (
         <>
-            <Starfield starCount={1000} starColor={[255, 255, 255]} speedFactor={0.05} backgroundColor="transparent"/>
+            <Starfield starCount={5000} starColor={[255, 255, 255]} speedFactor={0.05}/>
             <Header />
             <main>
                 <Home />
