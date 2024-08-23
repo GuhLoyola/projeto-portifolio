1340586d7aaf8ca7b13142777f07f992648ea931
Gustavo Loyola <guga.loyola17@gmail.com>
2024-08-22 23:50:05 -0300

    Componente Home2 criado

---
+++ src/components/Home/home-2.jsx
@@ -0,0 +1,12 @@
+const Home2 = () => {
+    return (
+        <section className="bg-black-russian h-screen text-smoky-white">
+            <div className="flex justify-center flex-col text-center">
+                <h2 className="text-4xl mb-20">Deixe eu me apresentar</h2>
+                <p className="container mx-auto px-30 mb-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, optio commodi dolorum tenetur, voluptatem officia omnis natus doloremque molestiae sint ipsum? Fugit ipsa explicabo qui, ea fugiat reiciendis omnis nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique omnis blanditiis nemo praesentium quibusdam consequuntur dignissimos perferendis accusantium veritatis voluptatum in, placeat rerum, sint veniam suscipit porro hic. Magnam, veritatis? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium quidem eum repellat ipsam officia veniam rerum enim modi hic fuga. Veritatis at dolorum eius ab debitis tenetur est eaque tempore?</p>
+            </div>
+        </section>
+    )
+}
+
+export default Home2
