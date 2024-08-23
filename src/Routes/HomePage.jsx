0e25a1215ad21d79e1e13e7403408d6b94782c26
Gustavo Loyola <guga.loyola17@gmail.com>
2024-08-22 23:51:34 -0300

    Componentes de Footer e Home2 adicionados a composição da homepage

--- src/Routes/HomePage.jsx
+++ src/Routes/HomePage.jsx
@@ -1,15 +1,19 @@
 import Starfield from "react-starfield"
 import Header from "../components/Header"
 import Home from "../components/Home/home"
+import Home2 from "../components/Home/home-2"
+import Footer from "../components/Footer"
 
 const HomePage = () => {
     return (
         <>
-            <Starfield starCount={5000} starColor={[255, 255, 255]} speedFactor={0.05}/>
+            <Starfield starCount={5000} starColor={[255, 255, 255]} speedFactor={0.05} />
             <Header />
             <main>
                 <Home />
+                <Home2 />
             </main>
+            <Footer />
         </>
     )
 }
