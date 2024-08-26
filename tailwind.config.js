b7dfd44bc97306a0dadbd5fcfae2524032e8cd79
Gustavo Loyola <guga.loyola17@gmail.com>
2024-08-25 21:33:49 -0300

    Cores inutilizadas removidas e spacing adicionado

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,20 +11,14 @@ export default {
         "black-russian": "#121212",
         "black-russian-66": "#121212a9",
         "cod-gray": "#282828",
-        "mine-shaft": "#3f3f3f",
-        "dove-gray": "#575757",
-        "boulder": "#717171",
-        "gray-light": "#8b8b8b",
         "blue-violet": "#8a2be2",
-        "medium-purple": "#9a48e6",
-        "amethyst": "#a960ea",
-        "lavender": "#b777ee",
-        "bright-lavender": "#c58ef1",
-        "alice-blue": "#d1e4f4",
         'smoky-white': "#f5f5f5"
       },
       fontFamily: {
         'poppins': 'Poppins'
+      },
+      spacing: {
+        '4.5': '1.125rem'
       }
     },
   },
