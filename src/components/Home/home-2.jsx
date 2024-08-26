46d6d66103d0bb4ab2ec43bc913f2205706e3d57
Gustavo Loyola <guga.loyola17@gmail.com>
2024-08-25 21:35:23 -0300

    Adicionada texto de apresentação breve e redes sociais

--- src/components/Home/home-2.jsx
+++ src/components/Home/home-2.jsx
@@ -1,9 +1,53 @@
+import { FaGithub, FaInstagram, FaLinkedinIn, FaWhatsapp } from "react-icons/fa"
+
 const Home2 = () => {
+
+    let Medias = [
+        { name: 'Instagram', link: 'https://www.instagram.com/g_loyola17', icon: <FaInstagram className="media-icon" />, id: 1 },
+        {
+            name: 'Linkedin', link: 'https://www.linkedin.com/in/gustavo-loyola-754102241',
+            icon: <FaLinkedinIn className="media-icon" />,
+            id: 2
+        },
+        { name: 'Github', link: 'https://github.com/GuhLoyola', icon: <FaGithub className="media-icon" />, id: 3 },
+        { name: 'Whatsapp', link: 'https://wa.me/5519995269890', icon: <FaWhatsapp className="media-icon" />, id: 4 },
+    ]
+
     return (
-        <section className="bg-black-russian h-screen text-smoky-white">
+        <section className="bg-black-russian text-smoky-white overflow-hidden">
             <div className="flex justify-center flex-col text-center">
-                <h2 className="text-4xl mb-20">Deixe eu me apresentar</h2>
-                <p className="container mx-auto px-30 mb-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, optio commodi dolorum tenetur, voluptatem officia omnis natus doloremque molestiae sint ipsum? Fugit ipsa explicabo qui, ea fugiat reiciendis omnis nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique omnis blanditiis nemo praesentium quibusdam consequuntur dignissimos perferendis accusantium veritatis voluptatum in, placeat rerum, sint veniam suscipit porro hic. Magnam, veritatis? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium quidem eum repellat ipsam officia veniam rerum enim modi hic fuga. Veritatis at dolorum eius ab debitis tenetur est eaque tempore?</p>
+                <h2 className="text-2xl mb-10 md:text-4xl md:mb-20">Deixe-me me apresentar</h2>
+                <div className="container mx-auto px-10 mb-10 md:mb-20 md:w-10/12 text-justify">
+                    <p className="text-sm md:text-lg leading-relaxed md:leading-normal">
+                        Sou um desenvolvedor front end, apaixonado por tecnologia e desenvolvimento web 👨🏻‍💻.
+                        <br />
+                        <br />
+                        Aqui, você encontrará alguns dos meus trabalhos favoritos, onde aplico minhas habilidades em desenvolver aplicações com <span className="text-blue-violet">Node.js</span> e frameworks Javascript como <span className="text-blue-violet">React.js</span>.
+                    </p>
+                    <p className="text-sm md:text-lg mt-5 leading-relaxed">
+                        Seja bem-vindo ao meu portifólio!
+                        <br />
+                        <span className="font-bold text-blue-violet italic">Vamos criar algo incrível juntos</span> 💜.
+                    </p>
+                </div>
+
+
+                <div className="text-center">
+                    <h2 className="text-2xl md:text-4xl mb-0 md:mb-2">Você pode me encontrar em</h2>
+                    <h3 className="md:text-xl">Sinta-se à vontade para se <span className="text-blue-violet">conectar</span> comigo</h3>
+                </div>
+
+                <ul className="grid grid-cols-2 gap-5 place-items-center py-10 md:flex md:justify-center md:items-center md:py-20">
+                    {
+                        Medias.map(media => (
+                            <li key={media.id} className="bg-cod-gray w-16 h-16 rounded-lg shadow-smoky-white shadow-md hover:shadow-blue-violet transition-all duration-300 ease-in-out hover:scale-110 z-30">
+                                <a href={media.link} target="_blank">
+                                    {media.icon}
+                                </a>
+                            </li>
+                        ))
+                    }
+                </ul>
             </div>
         </section>
     )
