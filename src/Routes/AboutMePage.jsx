3da8171e92d337cb2cf009b0932e075948c23da4
Gustavo Loyola <guga.loyola17@gmail.com>
2024-08-28 01:53:41 -0300

    Páginas das rotas criadas

---
+++ src/Routes/AboutMePage.jsx
@@ -0,0 +1,9 @@
+const AboutMePage = () => {
+    return (
+        <>
+            <h1>About Page 🙂</h1>
+        </>
+    )
+}
+
+export default AboutMePage
---
+++ src/Routes/ProjectPage.jsx
@@ -0,0 +1,7 @@
+const ProjectPage = () => {
+    return (
+        <h1>Project page</h1>
+    )
+}
+
+export default ProjectPage
