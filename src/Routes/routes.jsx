751b15b392cbb1f9a3b636badcc560b39b4a0233
Gustavo Loyola <guga.loyola17@gmail.com>
2024-08-28 02:00:41 -0300

    rotas modificadas

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import HomePage from "./HomePage"
-import AboutMePage from "./AboutMePage"
-import ContactPage from "./ContactPage"
-import ProjectPage from "./ProjectPage"
+import HomePage from "./homePage"
+import ContactPage from "./contactPage"
+import ProjectPage from "./projectPage"
+import AboutPage from "./aboutPage"
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/sobre-mim" element={<AboutMePage />} />
+                <Route path="/sobre-mim" element={<AboutPage />} />
                 <Route path="/projetos" element={<ProjectPage />} />
                 <Route path="/contato" element={<ContactPage />} />
             </Routes>
