3505f570048c0e0ac2db379474a886fbbc71f930
Gustavo Loyola <guga.loyola17@gmail.com>
2024-08-28 01:53:30 -0300

    Rotas adicionadas

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePage from "./HomePage"
-
+import AboutMePage from "./AboutMePage"
+import ContactPage from "./ContactPage"
+import ProjectPage from "./ProjectPage"
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/sobre-mim" element={<AboutMePage />} />
+                <Route path="/projetos" element={<ProjectPage />} />
+                <Route path="/contato" element={<ContactPage />} />
             </Routes>
         </BrowserRouter>
     )
